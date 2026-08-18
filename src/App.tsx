c582fad76133f9f1d87e68add744d7a304f7b481
Martin Baran <martin.baran12@gmail.com>
2026-08-18 15:32:35 +0200

    Update packages

--- src/App.tsx
+++ src/App.tsx
@@ -36,12 +36,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter
-          future={{
-            v7_relativeSplatPath: true,
-            v7_startTransition: true
-          }}
-        >
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/portfolio" element={<Portfolio />} />
